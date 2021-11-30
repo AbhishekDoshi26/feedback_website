10f91b12a19c898cb67ddae1a80d5f378f2a0a20
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2021-11-30 18:58:33 +0530

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:feedback_website/constants/config_constants.dart';
-import 'package:feedback_website/screens/home.dart';
+import 'package:feedback_website/screens/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -17,9 +17,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Feedback Website - Abhishek Doshi',
-      home: Home(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/screens/splash.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+import 'package:feedback_website/constants/contact_constants.dart';
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController logoController;
+  late Animation logoAnimation;
+  @override
+  void initState() {
+    super.initState();
+    logoController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    logoController.forward(); //Proceeds animation forward
+    logoController.addListener(() {
+      setState(() {});
+    });
+    logoAnimation = CurvedAnimation(
+      parent: logoController,
+      curve: Curves.bounceInOut,
+    );
+
+    Timer(
+      const Duration(seconds: 4),
+      () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const Home(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Hero(
+              tag: 'logo',
+              child: FlutterLogo(
+                size: logoAnimation.value *
+                    MediaQuery.of(context).size.height /
+                    2,
+              ),
+            ),
+            ShaderMask(
+              blendMode: BlendMode.srcIn,
+              shaderCallback: (bounds) => LinearGradient(colors: [
+                Colors.blue.shade400,
+                Colors.blue.shade900,
+              ]).createShader(
+                Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+              ),
+              child: Text(
+                ContactDetails.name,
+                style: TextStyle(
+                  fontSize: logoAnimation.value *
+                      (MediaQuery.of(context).size.width / 20),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
